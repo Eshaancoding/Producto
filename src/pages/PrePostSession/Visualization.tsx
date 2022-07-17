6524b75ea2dda365fa2927f41bd8a0ad13a3567d
Eshaan Barkataki <39027204+Eshaancoding@users.noreply.github.com>
2022-07-17 15:24:37 -0400

    Updated Visualize info

--- src/pages/PrePostSession/Visualization.tsx
+++ src/pages/PrePostSession/Visualization.tsx
@@ -17,13 +17,14 @@ function Visualization (props:any) {
     return (
         <IonPage> 
             <IonContent>
-                <CountBar minutes={0} seconds={45} useStartTime finish={() => {setColor("primary")}}/> 
+                <CountBar minutes={1} seconds={0} useStartTime finish={() => {setColor("primary")}}/> 
                 <IonTitle id="Title">Visualization</IonTitle>    
                 <IonText>
                     <p id="Description">
                         You should visualize <strong>two</strong> things. <br /> <br />
                         <strong>1. </strong> Visualize your <strong>failure</strong>. Imagine the things that will happen if you wouldn't do the habit that you want. For example, visualize the how you are not able to buy the dream car that you want because you haven't had good financial habits such as saving money. <br /> <br />
-                        <strong>2. </strong> Visualize the specific sequence of steps to <strong>get started</strong> on your habit. Think about what you need to do! For example, if you were studying for a math test, you would visualize getting a pencil out, getting your textbook from your backpack and then place it on the table. Then you would visualize going to the chapter that the test is on, and visualize sitting down and focusing your eyes on the textbook.
+                        <strong>2. </strong> Visualize the specific sequence of steps to <strong>get started</strong> and <strong>how you feel after doing</strong>the habit. Think about what you need to do! For example, if you were studying for a math test, you would visualize getting a pencil out, getting your textbook from your backpack and then place it on the table. Then you would visualize going to the chapter that the test is on, and visualize sitting down and focusing your eyes on the textbook. Also think about how you feel after you have done the habit! Visualize how accomplished and glad you will be after the habit<br />
+                        Note that the <strong>going into</strong> the habit and what you <strong>feel after</strong> the habit should be considered the <strong>entirity</strong> of the habit itself!
                     </p>
                 </IonText>
                 <IonButton id="Continue" color={color} onClick={handleContinue}>Continue</IonButton>
