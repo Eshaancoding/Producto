4d35700ff41fce04849fc1da980bdd9581a4cd93
Eshaan Barkataki <39027204+Eshaancoding@users.noreply.github.com>
2022-10-10 21:12:33 -0400

    First Commit - Cleaned rpeo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Redirect, Route } from 'react-router-dom';
-import { CreateAnimation, IonApp, IonRouterOutlet, setupIonicReact} from '@ionic/react';
+import { IonApp, IonRouterOutlet, setupIonicReact} from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home/Home';
 import TaskSelection from './pages/PrePostSession/TaskSelection'; 
@@ -7,8 +7,6 @@ import WorkSession from './pages/Sessions/WorkSession';
 import BreakSession from './pages/Sessions/BreakSession';
 import HabitReflection from './pages/Home/HabitReflection';
 import EditingHabitModal from './pages/Home/Editing/Habit';
-import Failure from './pages/PrePostSession/Failure';
-import BigPicture from './pages/PrePostSession/TheBigPicture';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -30,19 +28,6 @@ import '@ionic/react/css/display.css';
 import './theme/variables.css';
 import CreateReminder from './pages/Home/Editing/Reminder';
 
-/* Tips imports */
-import { TipOne } from './pages/Tips/TipOne';
-import { TipTwo } from './pages/Tips/TipTwo';
-import { TipThree } from './pages/Tips/TipThree';
-import { TipFour } from './pages/Tips/TipFour';
-import { TipFive } from './pages/Tips/TipFive';
-import { TipSix } from './pages/Tips/TipSix';
-import { TipSeven } from './pages/Tips/TipSeven';
-import { TipEight } from './pages/Tips/TipEight';
-import { TipNine } from './pages/Tips/TipNine';
-import { TipTen } from './pages/Tips/TipTen';
-import { QAPage } from './pages/Quiz/QAPage'
-
 setupIonicReact();
 
 const App: React.FC = () => (
@@ -54,8 +39,6 @@ const App: React.FC = () => (
         <Route exact path="/BreakSession" component={BreakSession} />
 
         <Route exact path="/taskSelect" component={TaskSelection} />
-        <Route exact path="/Failure" component={Failure} />
-        <Route exact path="/BigPicture" component={BigPicture} />
 
         <Route exact path="/HabitReflection" component={HabitReflection} />
 
@@ -72,23 +55,6 @@ const App: React.FC = () => (
           <CreateReminder create={false} />
         </Route>
 
-        {/* Tips! */}
-        <Route exact path="/TipOne" component={TipOne} />
-        <Route exact path="/TipTwo" component={TipTwo} />
-        <Route exact path="/TipThree" component={TipThree} />
-        <Route exact path="/TipFour" component={TipFour} />
-        <Route exact path="/TipFive" component={TipFive} />
-        <Route exact path="/TipSix" component={TipSix} />
-        <Route exact path="/TipSeven" component={TipSeven} />
-        <Route exact path="/TipEight" component={TipEight} />
-        <Route exact path="/TipNine" component={TipNine} />
-        <Route exact path="/TipTen" component={TipTen} />
-
-        <Route exact path="/QA" component={QAPage} />
-        <Route exact path="/QABreakSession">
-          <QAPage useCountBar />
-        </Route>
-        
         <Redirect exact path="/" to="/home" />
       </IonRouterOutlet>
     </IonReactRouter>
